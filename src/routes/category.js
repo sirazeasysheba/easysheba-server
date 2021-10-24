b2d803b915c8c5e4b02ccc30c63c63644c58aeab
sirazeasysheba <siraz@easysheba.com>
2021-10-24 17:58:41 +0600

    24th

--- src/routes/category.js
+++ src/routes/category.js
@@ -5,7 +5,11 @@ const router = express.Router();
 const multer = require("multer");
 const shortId = require("shortid");
 const path = require("path");
-const { addCategory, getCategories } = require("../controllers/category");
+const {
+  addCategory,
+  getCategories,
+  updateCategories,
+} = require("../controllers/category");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -27,5 +31,10 @@ router.post(
   addCategory
 );
 
-// router.get("/category/getcategory", getCategories);
+router.get("/category/getcategory", getCategories);
+router.post(
+  "/category/update",
+  upload.array("categoryImage"),
+  updateCategories
+);
 module.exports = router;
